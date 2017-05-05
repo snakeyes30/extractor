c34574ffa09a10b5baf401093aa951bc17bc349e
Sameer Shastri <sameer.shastri@betgenius.com>
2017-05-05 21:00:35 +0100

    unit test to get files as a tree

--- src/test/java/org/sam/extractor/Configuration/ApplcationConfigurationTest.java
+++ src/test/java/org/sam/extractor/Configuration/ApplcationConfigurationTest.java
@@ -1,8 +1,10 @@
 package org.sam.extractor.Configuration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.Test;
 
-import java.io.File;
 import java.util.*;
 
 /**
@@ -11,32 +13,68 @@ import java.util.*;
 public class ApplcationConfigurationTest {
 
 
+    private ObjectMapper objectMapper;
+
     @Test
     public void name() throws Exception {
         List<String> strings = Arrays.asList("C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E01.The.Good.Son.DVDRip.XviD-VF\\frasier.s01e01.dvdrip.xvid-vf.rar",
-                "C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E02.Space.Quest.DVDRip.XviD-VF\\frasier.s01e02.dvdrip.xvid-vf.rar", " C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E03.Dinner.At.Eight.DVDRip.XviD-VF\\frasier.s01e03.dvdrip.xvid-vf.rar", " C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E05.Heres.Looking.At.You.DVDRip.XviD-VF\\frasier.s01e05.dvdrip.xvid-vf.rar", " C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\frasier.s01e05.dvdrip.xvid-vf.rar");
+                "C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E02.Space.Quest.DVDRip.XviD-VF\\frasier.s01e02.dvdrip.xvid-vf.rar", "C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E03.Dinner.At.Eight.DVDRip.XviD-VF\\frasier.s01e03.dvdrip.xvid-vf.rar", "C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\Frasier.S01.DVDRip.XviD-SCC\\Frasier.S01E05.Heres.Looking.At.You.DVDRip.XviD-VF\\frasier.s01e05.dvdrip.xvid-vf.rar", "C:\\Users\\rshastri\\Desktop\\Frasier.S01-S11.DVDRip.XviD-SCC\\frasier.s01e05.dvdrip.xvid-vf.rar");
 
+
+//        List<String> strings = Arrays.asList("C:\\Users\\", "C:\\Users\\Cheese", "C:\\Users\\Cake");
         Map<String, List> map = new HashMap<>();
         strings.forEach(s -> {
-                    String[] split = s.split(File.pathSeparator);
-                    for (int i = 0; i < split.length; i++) {
-                        String sp = split[i];
-                        String next = null;
-                        if (i + 1 < split.length) {
-                            next = split[i + 1];
+                    String[] split = s.split("\\\\");
+//                    for (int i = 0; i < split.length; i++) {
+//                        String sp = split[i];
+                        try {
+                            getList(map, 0, split);
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
                         }
-                        List list = getList(map, i, split);
 
-                    }
+//                    }
                 }
         );
+        System.out.println(map);
+        objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        System.out.println(objectMapper.writeValueAsString(map));
 
     }
 
-    private List getList(Map<String, List> map, int sp, String[] next) {
-        if (next == null) {
-            return new ArrayList();
+    /**
+     * {
+     * "C": [
+     * `        "Users": [
+     * <p>
+     * ]
+     * ]
+     * }
+     *
+     * @param map
+     * @param sp
+     * @param next
+     * @return
+     */
+    private Map getList(Map map, int sp, String[] next) throws JsonProcessingException {
+        if (sp >= next.length) {
+            return new HashMap();
         }
-        return map.computeIfAbsent(sp, s1 -> new ArrayList());
+        String nextSp = next[sp];
+        System.out.println("Working on file part " + nextSp);
+        Map newMap = new HashMap();
+        if (map.containsKey(nextSp)) {
+            newMap = (Map) map.get(nextSp);
+        }
+        Map list = getList(newMap, sp + 1, next);
+
+            map.put(nextSp, list);
+
+//        System.out.println(next[sp]);
+
+        return map;
+
     }
 }
